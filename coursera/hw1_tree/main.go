5cadbb9e6f8a3083b243fbaa28962bd412b63d5f
se-forex <m.m.jur@ya.ru>
2018-03-26 17:17:40 +0300

    add recur func

--- coursera/hw1_tree/main.go
+++ coursera/hw1_tree/main.go
@@ -10,20 +10,91 @@ import (
 	// "strings"
 )
 
+func multiStr(s string, num int) string {
+	var newStr string
+	for i := 0; i < num; i++ {
+		newStr = newStr + s
+	}
+	return newStr
+}
+
+// func shTree(path string, count int) error {
+// 	files, err := ioutil.ReadDir(path)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	//fmt.Println(len(files))
+
+// 	for _, file := range files {
+// 		if file.IsDir() {
+// 			fmt.Println("├───", file.Name())
+// 			err := dirTree(path + "/" + file.Name())
+// 			if err != nil {
+// 				continue
+// 			}
+// 		} else {
+// 			fmt.Println("│  └───", file.Name())
+// 		}
+// 	}
+// 	return nil
+// }
+
+func printDir(sep string, level int) {
+	fmt.Println(multiStr("a", level))
+}
+
+func getDir(path string) (names []os.FileInfo, count int, err error) {
+	names, err = ioutil.ReadDir(path)
+	if err != nil {
+		return nil, 0, err
+	}
+	count = len(names)
+	return names, count, err
+}
+
 func dirTree(path string) error {
-	files, err := ioutil.ReadDir(path)
+	var level int
+	//sep1, sep2 := "├───", "───"
+	names, count, err := getDir(path)
+
 	if err != nil {
 		return err
 	}
 
-	for _, file := range files {
-		fmt.Println(file.Name())
-
-		err := dirTree(path + "/" + file.Name())
-		if err != nil {
-			continue
+	for i := 0; i < count; i++ {
+		name := names[i]
+		if name.IsDir() {
+			fmt.Println(name)
+			level++
+			dirTree(path + "/" + name.Name())
 		}
 	}
+	level = 0
+
+	// sep1, sep2 := "├───", "└───"
+	// var count int
+
+	// shTree(path, count)
+	// files, err := ioutil.ReadDir(path)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// //fmt.Println(len(files))
+
+	// for _, file := range files {
+	// 	if file.IsDir() {
+	// 		fmt.Println("d├───", file.Name())
+	// 	} else {
+	// 		fmt.Println("f├───", file.Name())
+	// 	}
+
+	// 	err := dirTree(path + "/" + file.Name())
+	// 	if err != nil {
+	// 		continue
+	// 	}
+	// }
 	return nil
 }
 
@@ -38,6 +109,7 @@ func main() {
 	// if err != nil {
 	// 	panic(err.Error())
 	// }
+	fmt.Println(multiStr("a", 3))
 	fmt.Println("pfiles", printFiles)
 	dirTree(path)
 }

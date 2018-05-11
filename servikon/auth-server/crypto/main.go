57510ddf97b77d91cf8b8cafaac5d01d1baaf674
se-forex <m.m.jur@ya.ru>
2018-05-11 16:23:31 +0300

    add last symbol

--- servikon/auth-server/crypto/main.go
+++ servikon/auth-server/crypto/main.go
@@ -26,10 +26,14 @@ func genHashPass(p, s []byte) []byte {
 
 func main() {
 
+	sss := "a"
+
 	pass := "123123"
 	salt := genSalt(10)
 
-	newPass := "1231231"
+	serverSalt := "MIfpaOS&UE02j30*YdfQA(PE8fQP(#&@Uhsal"
+
+	newPass := "123123"
 
 	// io.WriteString(h, "love")
 	// st1 := h.Sum([]byte("love"))
@@ -45,13 +49,26 @@ func main() {
 	// fmt.Println(salt)
 	// fmt.Printf("%x \n", sha1.New().Sum([]byte(salt)))
 
-	hp1 := genHashPass(md5.New().Sum([]byte(pass)), sha1.New().Sum([]byte(salt)))
-	hp2 := genHashPass(md5.New().Sum([]byte(newPass)), sha1.New().Sum([]byte(salt)))
+	hp1 := genHashPass(genHashPass(md5.New().Sum([]byte(pass)), sha1.New().Sum([]byte(salt))), md5.New().Sum([]byte(serverSalt)))
+	hp2 := genHashPass(genHashPass(md5.New().Sum([]byte(newPass)), sha1.New().Sum([]byte(salt))), md5.New().Sum([]byte(serverSalt)))
 
 	fmt.Println("Initial passHash:")
 	fmt.Printf("%x \n", hp1)
+	// fmt.Print(len(hp1), " ")
+	// fmt.Print(len(sss), " ")
+	// fmt.Println(len(string(hp1) + sss))
+	// fmt.Printf("%x \n", string(hp1))
 	fmt.Println("New passHash:")
 	fmt.Printf("%x \n", hp2)
+
+	fmt.Print("len h2: ")
+	fmt.Printf("%x \n", len(string(hp2)))
+	fmt.Println("len sss:", len(sss))
+	fmt.Printf("%x \n", string(hp2)+sss)
+
+	nPass := string(hp2) + sss
+	fmt.Println("sss")
+	fmt.Printf("%x \n", nPass[:(len(nPass)-len(sss))])
 
 	if bytes.Equal(hp1, hp2) {
 		fmt.Println("\nPasswors equal")

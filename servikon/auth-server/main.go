4553c30ebd94b8246823280873012a822ae3bd9c
se-forex <m.m.jur@ya.ru>
2018-05-16 15:48:36 +0300

    add insert user to db

--- servikon/auth-server/main.go
+++ servikon/auth-server/main.go
@@ -38,6 +38,59 @@ func initDb() {
 	fmt.Println("DB successfully connected!")
 }
 
+func insertUserToDb(name, email, pass string) error {
+
+	stmt, err := db.Prepare(`
+		INSERT INTO records.users
+		(username, email, pass) VALUES ($1, $2, $3)`)
+	if err != nil {
+		//log.Fatal(err)
+		return err
+	}
+
+	res, err := stmt.Exec(name, email, pass)
+	if err != nil {
+		//log.Fatal(err)
+		return err
+	}
+
+	affect, err := res.RowsAffected()
+	if err != nil {
+		//log.Fatal(err)
+		return err
+	}
+
+	if err == nil {
+		log.Println("User successfully added, affected ", affect, " rows.")
+	}
+
+	return nil
+}
+
+func searchUserByEmail(email string) (User, error) {
+	user := new(User)
+
+	// Read data from DB
+	rows, errQr := db.Query(`
+		SELECT uid, username, email, pass 
+		FROM records.users WHERE email='` + email + "'")
+	if errQr != nil {
+		log.Fatal(errQr)
+		return *user, errQr
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		errSt := rows.Scan(&user.Uid, &user.Username, &user.Email, &user.Pass)
+		if errSt != nil {
+			log.Fatal(errSt)
+			return *user, errSt
+		}
+	}
+
+	return *user, nil
+}
+
 // Http server section
 func getUserByEmail(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()       // parse arguments, you have to call this by yourself
@@ -64,30 +117,39 @@ func getUserByEmail(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, string(out))
-
-	//fmt.Fprintf(w, "%s, %s, %s, %s\n", user.Uid, user.Username, user.Email, user.Pass) // send data to client side
 }
 
-func searchUserByEmail(email string) (User, error) {
+func createUser(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       // parse arguments, you have to call this by yourself
+	fmt.Println(r.Form) // print form information in server side
+
 	user := new(User)
+	// var err error
 
-	// Read data from DB
-	rows, errQr := db.Query("SELECT uid, username, email, pass FROM records.users WHERE email='" + email + "'")
-	if errQr != nil {
-		log.Fatal(errQr)
-		return *user, errQr
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		errSt := rows.Scan(&user.Uid, &user.Username, &user.Email, &user.Pass)
-		if errSt != nil {
-			log.Fatal(errSt)
-			return *user, errSt
+	for k, v := range r.Form {
+		switch {
+		case k == "name":
+			user.Username = strings.Join(v, "")
+		case k == "email":
+			user.Email = strings.Join(v, "")
+		case k == "pass":
+			user.Pass = strings.Join(v, "")
 		}
 	}
 
-	return *user, nil
+	err := insertUserToDb(user.Username, user.Email, user.Pass)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	} else {
+		out, err := json.Marshal(user)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+		}
+
+		fmt.Fprintf(w, "User successfully added: \n")
+		fmt.Fprintf(w, "%s\n", string(out))
+	}
+
 }
 
 func main() {
@@ -97,6 +159,7 @@ func main() {
 
 	// start Http server
 	http.HandleFunc("/get", getUserByEmail)  // set router
+	http.HandleFunc("/set", createUser)      // set router
 	err := http.ListenAndServe(":9090", nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
